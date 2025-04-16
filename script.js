f7ad29b7a46b0a27007bc584591d5b0903b965ce
Utkarsh Chaudhary <72092214+ukchaudhary-16@users.noreply.github.com>
2025-04-16 16:16:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const mybtn1=document.getElementById("button1") 
+mybtn1.addEventListener("click",function(){
+    window.location.href='wallet_gen.html'
+    
+    
+});
+
+const mybtn2= document.getElementById("button2")
+mybtn2.addEventListener("click",function(){
+    window.location.href='wallet_gen.html';
+});
